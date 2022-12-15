8ff5768e1b8a87f1ade1b5998aedbe5cfaad5f47
Igal Epshtein <igalep@Igals-MacBook-Pro.local>
2022-12-15 13:16:55 +0200

    adding firts route for /list

---
+++ convertor/Currency_convertor.py
@@ -0,0 +1,29 @@
+import requests as req
+import json
+import pprint
+
+class Currency_convertor:
+    rates = {}
+    def __init__(self, url , api_key):
+        self.url = url
+        self.api_key = api_key
+
+    def get_list(self):
+        ROUTE = '/list'
+        headers = {"apikey" : self.api_key}
+        response = req.get('http://' + self.url + ROUTE, headers=headers)
+        return response.json()
+
+
+
+
+if __name__ == "__main__":
+    with open("../config/config.json") as json_config:
+        config = json.load(json_config)
+        url = config["externalService"]["url"]
+        api_key = config["externalService"]["apikey"]
+
+        cc = Currency_convertor(url, api_key)
+        data = cc.get_list()
+
+        pprint.pprint(data)
